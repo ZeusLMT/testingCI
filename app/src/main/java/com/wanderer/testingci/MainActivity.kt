37c30b97d1c2ce6ec7f393439d6fa043fa0253ef
ZeusLMT <lemanhtuan275@gmail.com>
2018-10-24 10:17:46 +0300

    Initial commit

---
+++ app/src/main/java/com/wanderer/testingci/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.wanderer.testingci
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "testing", Toast.LENGTH_SHORT).show()
+    }
+}
