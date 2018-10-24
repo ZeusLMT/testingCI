53ff4a95ef28df8786bc99e40b442027dd58f6a2
ZeusLMT <lemanhtuan275@gmail.com>
2018-10-24 10:37:43 +0300

    error build

--- app/src/main/java/com/wanderer/testingci/MainActivity.kt
+++ app/src/main/java/com/wanderer/testingci/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "testing", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "testing 2", Toast.LENGTH_SHORT)
     }
 }
